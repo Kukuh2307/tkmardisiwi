941133096d9472b1712e98d6aa2f11b148180e95
Kukuh2307 <kukukhagung12@gmail.com>
2023-02-14 22:47:11 +0700

    (finish) dashboard admin

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+// koneksi database
+$koneksi = mysqli_connect("localhost","root","","db_tkmardisiwi");
+
+
+// cek koneksi
+// if(mysqli_connect_errno()){
+//     echo "gagal konek";
+// } else {
+//     echo "sukses";
+// }
+
+// url induk
+$main_url ="http://localhost/tkmardisiwi";
---
+++ user/add-user.php
@@ -0,0 +1,20 @@
+<?php
+require_once"../config.php";
+$tittle = "Tambah User - TK MARDISIWI";
+require_once"../template/header.php";
+require_once"../template/navbar.php";
+require_once"../template/sidebar.php";
+?>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">Tambah User</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item"><a href="<?=$main_url?>"class="link-secondary">Home</a></li>
+                            <li class="breadcrumb-item active">Tambah User</li>
+                        </ol>
+                    </div>
+                </main>
+<?php
+require_once"../template/footer.php";
+?>
