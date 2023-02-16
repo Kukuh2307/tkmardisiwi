78e763e8dd6fcd3578b64899cad969504d961184
Kukuh2307 <kukukhagung12@gmail.com>
2023-02-16 21:09:31 +0700

    (finish) profile-sekolah.php and (modify) backround navbar dan direct link

--- template/navbar.php
+++ template/navbar.php
@@ -1,5 +1,5 @@
 <body class="sb-nav-fixed">
-        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark">
             <!-- Navbar Brand-->
             <a class="navbar-brand ps-3" href="<?=$main_url?>index.php">TK MARDISIWI</a>
             <!-- Sidebar Toggle-->
@@ -11,10 +11,10 @@
             <!-- Navbar-->
             <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw text-black"></i></a>
+                    <ul class="dropdown-menu dropdown-menu-end text-black" aria-labelledby="navbarDropdown">
                         <li><a class="dropdown-item" href="#!">Profile User</a></li>
-                        <li><a class="dropdown-item" href="#!">Profile Sekolah</a></li>
+                        <li><a class="dropdown-item" href="<?=$main_url?>/otherElement/profile-sekolah.php">Profile Sekolah</a></li>
                         <li><hr class="dropdown-divider" /></li>
                         <li><a class="dropdown-item" href="#!">Logout</a></li>
                     </ul>
